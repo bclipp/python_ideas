89c842a1b2217d0ce8b1c8d0ba0f5954a9d2932e
blipp <bclipp770@gmail.com>
2020-04-23 07:03:30 -0400

    playing with standard python stubing

--- module_import_mock/test_modulea.py
+++ module_import_mock/test_modulea.py
@@ -33,3 +33,31 @@ def test_modulea(mocker):
     mocker.patch("modulea.time.sleep", return_value="This a a fake return value from time.sleep")
     print(modulea.do_something2())
     assert 1 == 1
+
+# using pytest-mock
+def test_modulea(mocker):
+    mocker.patch.object(modulea, "time")
+    mocker.ANY
+    modulea.do_something()
+    assert 1 == 1
+
+#using standard python
+
+from unittest.mock import Mock
+from unittest.mock import patch
+
+# stub
+@patch("modulea.time")
+def test_modulea_1(mock_time):
+    modulea.do_something()
+    assert 1 == 1
+
+# mock
+@patch("modulea.time")
+def test_modulea_1(mock_time):
+    modulea.do_something()
+    assert 1 == 1
+
+#from mock import Mock
+#mock = Mock()
+#print mock.call_count
